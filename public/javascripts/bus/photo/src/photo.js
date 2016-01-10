ac55e440a09a87a021e300b34f3220ae1f48f691
felling <fei.pan@dji.com>
2016-01-10 12:45:59 +0800

    add layer js of photo

--- public/javascripts/bus/photo/src/photo.js
+++ public/javascripts/bus/photo/src/photo.js
@@ -4,8 +4,15 @@
           $(this).toggleClass("gray");
       });
 
-      $(".photo-wrap img").lazyload({
-          attr:"data-original"
+      var layzr = new Layzr({
+        container: null,
+        selector: '[data-layzr]',
+        attr: 'data-layzr',
+        retinaAttr: 'data-layzr-retina',
+        bgAttr: 'data-layzr-bg',
+        hiddenAttr: 'data-layzr-hidden',
+        threshold: 50,
+        callback: null
       });
 
       $("#content .fancybox").fancybox({
